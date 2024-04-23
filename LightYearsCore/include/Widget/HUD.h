1b6396a61b646889edf4f668d160fe6d016b4bf1
Ahmed Yasser <ahmedyaserabbas@gmail.com>
2024-04-23 09:39:55 +0200

    Added the HUD class

---
+++ LightYearsCore/include/Widget/HUD.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include "Framework/Object.h"
+
+namespace ly
+{
+	class HUD : public Object
+	{
+	public:
+		virtual void Draw(sf::RenderWindow& window) = 0;
+		virtual bool HandleEvent(const sf::Event& event);
+
+		void NativeInit(sf::RenderWindow& window);
+		inline bool IsInit() const { return m_IsInit; }
+
+	protected:
+		HUD();
+
+	private:
+		virtual void Init(sf::RenderWindow& window);
+
+	private:
+		bool m_IsInit;
+	};
+}
---
+++ LightYearsCore/src/Widget/HUD.cpp
@@ -0,0 +1,27 @@
+#include "Widget/HUD.h"
+
+namespace ly
+{
+	bool HUD::HandleEvent(const sf::Event& event)
+	{
+		return false;
+	}
+
+	void HUD::NativeInit(sf::RenderWindow& window)
+	{
+		if (!m_IsInit)
+		{
+			m_IsInit = true;
+			Init(window);
+		}
+	}
+
+	HUD::HUD()
+		: m_IsInit{false}
+	{
+	}
+
+	void HUD::Init(sf::RenderWindow& window)
+	{
+	}
+}
